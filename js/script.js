f1bb65d3f971d547670cecbff4ff69d5423fdf28
Livio Simard <liviosimard@yahoo.fr>
2024-06-11 18:57:01 +0200

    logic for best movies section added in script.js

--- js/script.js
+++ js/script.js
@@ -1,49 +1,116 @@
 const bestMovieUrl = 'http://localhost:8000/api/v1/titles/?sort_by=-imdb_score';
 
-function bestMovieRequest(url) {
-  fetch(bestMovieUrl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Erreur');
-    }
-    return response.json();
-  })
-  .then(data => {
-    const dataBestMovieUrl = data.results[0].url;
-    console.log('best_movie_url: ', dataBestMovieUrl);
-
-    fetch(dataBestMovieUrl)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Erreur');
-        }
-        return response.json();
-      })
-      .then(data => {
-        const dataBestMovieImage = data.image_url;
-        const dataBestMovieTitle = data.title;
-        const dataBestMovieDescription = data.description;
-
-        console.log('best_movie_image: ', dataBestMovieImage)
-        console.log('best_movie_title: ', dataBestMovieTitle)
-        console.log('best_movie_description: ', dataBestMovieDescription)
-
-        const bestMovieImage = document.getElementById('best-movie-img');
-        const bestMovieTitle = document.getElementById('best-movie-title');
-        const bestMovieDescription = document.getElementById('best-movie-description');
-
-        bestMovieImage.src = dataBestMovieImage;
-        bestMovieTitle.textContent = dataBestMovieTitle;
-        bestMovieDescription.textContent = dataBestMovieDescription;
-      })
-      .catch(error => {
-        console.error('Erreur lors de la requête:', error);
-      });
-  })
-  .catch(error => {
-    console.error('Erreur lors de la requête:', error);
-  });
+async function bestMovieRequest(url) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error('Erreur response !');
+  }
+  const data = await response.json();
+  return data;
 }
 
-bestMovieRequest(bestMovieUrl);
+bestMovieRequest(bestMovieUrl)
+  .then(data => {
+    const dataBestsMovies = data.results;
+    console.log(dataBestsMovies);
 
+    let i = 0;
+    for (const movie of dataBestsMovies) {
+      fetchBestMovie(movie, i);
+      i++;
+    }
+  })
+  .catch(error => {
+    console.error('Erreur :', error);
+  });
+
+async function fetchBestMovie(movie, i) {
+  try {
+    console.log(movie.url);
+    console.log(i);
+
+    const response = await fetch(movie.url);
+    if (!response.ok) {
+      throw new Error('Erreur response !');
+    }
+    const data = await response.json();
+
+    if (i === 0) {
+      const image = data.image_url;
+      const title = data.title;
+      const description = data.description;
+
+      console.log('image: ', image);
+      console.log('title: ', title);
+      console.log('description: ', description);
+
+      const bestMovieImage = document.getElementById('best-movie-img');
+      const bestMovieTitle = document.getElementById('best-movie-title');
+      const bestMovieDescription = document.getElementById('best-movie-description');
+
+      bestMovieImage.src = image;
+      bestMovieTitle.textContent = title;
+      bestMovieDescription.textContent = description;
+    }
+
+    if (i === 1) {
+      const image = data.image_url;
+      const title = data.title;
+
+      console.log('image: ', image);
+      console.log('title: ', title);
+
+      const bestMovieImage = document.getElementById('best-movie-img-1');
+      const bestMovieTitle = document.getElementById('best-movie-title-1');
+
+      bestMovieImage.src = image;
+      bestMovieTitle.textContent = title;
+    }
+
+    if (i === 2) {
+      const image = data.image_url;
+      const title = data.title;
+
+      console.log('image: ', image);
+      console.log('title: ', title);
+
+      const bestMovieImage = document.getElementById('best-movie-img-2');
+      const bestMovieTitle = document.getElementById('best-movie-title-2');
+
+      bestMovieImage.src = image;
+      bestMovieTitle.textContent = title;
+    }
+
+    if (i === 3) {
+      const image = data.image_url;
+      const title = data.title;
+
+      console.log('image: ', image);
+      console.log('title: ', title);
+
+      const bestMovieImage = document.getElementById('best-movie-img-3');
+      const bestMovieTitle = document.getElementById('best-movie-title-3');
+
+      bestMovieImage.src = image;
+      bestMovieTitle.textContent = title;
+    }
+
+    if (i === 4) {
+      const image = data.image_url;
+      const title = data.title;
+
+      console.log('image: ', image);
+      console.log('title: ', title);
+
+      const bestMovieImage = document.getElementById('best-movie-img-4');
+      const bestMovieTitle = document.getElementById('best-movie-title-4');
+
+      bestMovieImage.src = image;
+      bestMovieTitle.textContent = title;
+    }
+
+    console.log(data.title);
+  } catch (error) {
+    console.error('Erreur :', error);
+  }
+}
